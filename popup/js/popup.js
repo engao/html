08e31f87a203f94d45a292241827bf921aad2b1b
engao <1067547248@qq.com>
2017-10-22 19:22:14 +0800

    弹窗,如有不了解的，欢迎来咨询

---
+++ popup/js/popup.js
@@ -0,0 +1,57 @@
+var kelPopul = function(data, html){
+	var initWidth = data.width;
+	var initHeight = data.height;
+	var initTitle = data.title;
+
+	var kjStr = '<div id="kel-mask"></div>';
+		kjStr += '<div id="kel-popul">';
+		kjStr += '<div class="kel_popul_title">';
+		kjStr += '<span class="kel_popul_span">自定义标题</span>';
+		kjStr += '<a href="javascript:;" class="kel_popul_close">x</a>';
+		kjStr += '</div>';
+		kjStr += '<div class="kel_popul_content"></div>';
+		kjStr += '</div>';
+
+	$('body').append(kjStr);
+	$('.kel_popul_content').html(html);
+	$('.kel_popul_span').html(initTitle);
+	$('#kel-popul').css({"height":initHeight,"width":initWidth,"margin-left":"-"+initWidth/2+"px","margin-top":"-"+initHeight/2+"px"});
+
+	this.run = function(){
+		$('#showPopul').click(function(){
+			$('#kel-mask').show();
+			$('#kel-popul').show();
+		});
+
+		$('#kel-mask').click(function(){
+			$(this).hide();
+			$('#kel-popul').hide();
+		});
+
+		$('.kel_popul_close').click(function(){
+			$('#kel-mask').hide();
+			$('#kel-popul').hide();
+		});
+
+		//移动窗口
+		$(document).on('mousedown', '.kel_popul_title', function(ev){
+			var initPopulLeft =  $('#kel-popul').offset().left;
+			var initPopulTop = $('#kel-popul').offset().top;
+			var initX = ev.pageX - initPopulLeft;
+			var initY = ev.pageY - initPopulTop;
+			
+			var isMove = true; 
+			$(document).mousemove(function(nev){
+				
+				if(isMove){
+					var nX = nev.pageX - initX;
+					var nY = nev.pageY - initY;
+
+					$('#kel-popul').css({"left":nX,"top":nY, 'margin-left':'0px', 'margin-top':'0px'});
+				}
+			}).mouseup(function(){
+				isMove = false; 
+			});
+		});
+	}
+}
